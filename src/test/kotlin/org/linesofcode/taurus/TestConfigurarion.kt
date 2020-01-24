cbd7b715cf6f676f896fed6cae2ae0148246219d
Jonas Jurczok <jonas.jurczok@akelius.de>
2020-01-24 14:19:16 +0100

    move AdminClient to testconfig

--- src/test/kotlin/org/linesofcode/taurus/TestConfigurarion.kt
+++ src/test/kotlin/org/linesofcode/taurus/TestConfigurarion.kt
@@ -1,10 +1,12 @@
 package org.linesofcode.taurus
 
+import org.apache.kafka.clients.admin.AdminClient
 import org.linesofcode.taurus.infrastructure.AbstractReadFromBeginningListenerTest
 import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Scope
+import org.springframework.kafka.core.KafkaAdmin
 
 @Configuration
 class TestConfigurarion {
@@ -15,4 +17,8 @@ class TestConfigurarion {
         return AbstractReadFromBeginningListenerTest.TestListener()
     }
 
+    @Bean
+    fun kafkaAdminClient(admin: KafkaAdmin): AdminClient {
+        return AdminClient.create(admin.config)
+    }
 }
--- src/test/kotlin/org/linesofcode/taurus/infrastructure/AbstractReadFromBeginningListenerTest.kt
+++ src/test/kotlin/org/linesofcode/taurus/infrastructure/AbstractReadFromBeginningListenerTest.kt
@@ -2,10 +2,8 @@ package org.linesofcode.taurus.infrastructure
 
 import org.apache.kafka.clients.admin.AdminClient
 import org.apache.kafka.clients.admin.NewTopic
-import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
@@ -14,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry
-import org.springframework.kafka.core.KafkaAdmin
 import org.springframework.kafka.core.KafkaTemplate
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
@@ -30,7 +27,7 @@ class AbstractReadFromBeginningListenerTest {
     private lateinit var kafkaTemplate: KafkaTemplate<String, String>
 
     @Autowired
-    private lateinit var kafkaAdmin: KafkaAdmin
+    private lateinit var adminClient: AdminClient
 
     @Autowired
     private lateinit var applicationContext: ApplicationContext
@@ -42,14 +39,12 @@ class AbstractReadFromBeginningListenerTest {
     @BeforeEach
     fun setup() {
         logger.info("Creating topic [{}].", topicName)
-        val adminClient = AdminClient.create(kafkaAdmin.config)
         adminClient.createTopics(listOf(NewTopic(topicName, 1, 1)))
     }
 
     @AfterEach
     fun teardown() {
         logger.info("Deleting topic [{}].", topicName)
-        val adminClient = AdminClient.create(kafkaAdmin.config)
         adminClient.deleteTopics(listOf(topicName))
     }
 
