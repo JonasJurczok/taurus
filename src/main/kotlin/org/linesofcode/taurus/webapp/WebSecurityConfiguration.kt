c61e7d4df8833c572f92a3c9d557aeda6ee5dd0b
Jonas Jurczok <jonasjurczok@gmail.com>
2020-01-29 23:54:54 +0100

    added basic UI

--- src/main/kotlin/org/linesofcode/taurus/webapp/WebSecurityConfiguration.kt
+++ src/main/kotlin/org/linesofcode/taurus/webapp/WebSecurityConfiguration.kt
@@ -1,38 +1,41 @@
 package org.linesofcode.taurus.webapp
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @EnableWebSecurity
 class WebSecurityConfiguration: WebSecurityConfigurerAdapter() {
 
-    @Autowired
-    private lateinit var authenticationSuccessHandler: AuthenticationSuccessHandler
-
     override fun configure(http: HttpSecurity?) {
         http?.authorizeRequests()
-                ?.antMatchers("/", "/login", "/css/**", "/js/**", "/images/**")?.permitAll()
-                ?.antMatchers("/**")?.authenticated()
+                ?.antMatchers("/", "/login", "/css/**", "/js/**", "/img/**", "/webjars/**")?.permitAll()
+                ?.anyRequest()?.authenticated()
                 ?.and()
-                ?.formLogin()
-                ?.loginPage("/login")
-                ?.loginProcessingUrl("/login")
-                ?.failureUrl("/login?login_error")
-                ?.successHandler(authenticationSuccessHandler);
+                    ?.formLogin()
+                    ?.successForwardUrl("/?login_success")
+                    ?.failureForwardUrl("/?login_error&login")
+                    ?.loginPage("/?login")
+                    ?.permitAll()
+                ?.and()
+                ?.logout()
+                    ?.invalidateHttpSession(true)
+                    ?.clearAuthentication(true)
+                    ?.logoutRequestMatcher(AntPathRequestMatcher("/logout"))
+                    ?.logoutSuccessUrl("/login?logout")
+                    ?.permitAll()
     }
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
         auth?.inMemoryAuthentication()
-                ?.withUser("user")
-                ?.password("user")
+                ?.withUser("alice@example.com")
+                ?.password("alice")
                 ?.roles("USER")
                 ?.and()
-                ?.withUser("admin")
-                ?.password("admin")
-                ?.roles("ADMIN");
+                ?.withUser("bob@example.com")
+                ?.password("bob")
+                ?.roles("USER")
     }
 }
